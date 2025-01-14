b9d459bc422fc8dd9c4324d4c5ee2bbd91adad70
Parsa Aysham <parsaaysham@Parsas-Mac-mini.local>
2025-01-15 03:03:10 +0330

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Splash from './SplashScren';
+function App() {
+  return (
+    <>
+    <Splash/>
+    
+    </>
+  );
+}
+
+export default App;
---
+++ src/SplashScren.js
@@ -0,0 +1,25 @@
+import './SplashScren.css';
+import eye from './Logo.svg';
+import text from './Group 3.svg'
+function Splash(){
+
+
+
+return(
+<>
+<div className='head'>
+<div className='container'>
+<img src={eye} className='eye-logo'/>
+<img src={text} className='text-logo'/>
+
+</div>
+
+</div>
+
+</>
+
+
+)
+
+}
+export default Splash;
