4325136e6aecc45cb3b9f1554196c89e06d5c9a8
parsaempir <parsa.aysham54@gmail.com>
2025-01-16 05:04:41 +0330

    ada

--- src/StatusPage.js
+++ src/StatusPage.js
@@ -7,12 +7,25 @@ import Back from './Group.svg';
 import giah from './logo gia.png';
 import addicon from './کارت - اضافه کردن بازیکن.png';
 import icon from './پشت کارت.png';
-import iconzarb from './Group 6.png'
+import iconzarb from './Group 6.png';
+import jasosin from './کارت - جاسوس.png';
+import addjas from './کارت - اضافه کردن جاسوس.png'
 function StatusPage(){
 
     const [players, setPlayers] = useState([]);
 
-      
+    const [jasos, setJasos] = useState([]);
+    const addJasos =  () => {
+if (jasos.length <2) {
+const newJasos = { id: Date.now(), name: `جاسوس ${jasos.length + 1}`};
+setJasos([...jasos, newJasos]);
+} else{
+    alert("فقط ۲جاسوس می توانید اضافه کنید")
+}
+    };
+    const removeJasos =(id) => {
+        setJasos(jasos.filter((jas) => jas.id !== id ));
+    };
     const addPlayer = () => {
         if (players.length < 4) { 
           const newPlayer = { id: Date.now(), name: `بازیکن ${players.length + 1}` };
@@ -74,8 +87,43 @@ src={iconzarb}
         ))}
       </div>
     </div>
-    <div className='hr'></div>   
+    <div className='hr'></div> 
+    <span className='h-head' id='h-head'>
+<h1 className='h-text' >تعداد جاسوس ها</h1>
+<div className="player-count">{jasos.length}</div>
+</span>
+ 
+    <div className="player-container" id='player-container'>
+      <div className="player-list">
+        {}
+        <img
+          src={addjas}
+          alt="افزودن بازیکن"
+          className="add-player-icon"
+          onClick={addJasos}
+          height='100px'
+        />
 
+        {}
+        {jasos.map((jas) => (
+          <div key={jas.id} className="player-card">
+          
+          <img 
+                        onClick={() => removeJasos(jas.id)}
+src={iconzarb}
+          className='remove-btn'
+          />
+            <img
+              src={jasosin} 
+              alt={jas.name}
+              className="player-img"
+              height='100px'
+            />
+          </div>
+        ))}
+      </div>
+    </div>
+    <div className='hr' id='hr'></div> 
 
 <img src={giah} className='giah'/>
 </div>
