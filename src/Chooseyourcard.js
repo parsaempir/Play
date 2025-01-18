c68279431eeeebded9a3c09aa7a975caf68cd963
parsaempir <parsa.aysham54@gmail.com>
2025-01-18 05:12:30 +0330

    /.

--- src/Chooseyourcard.js
+++ src/Chooseyourcard.js
@@ -72,7 +72,56 @@ function Chooseyourcard() {
         </div>
       </div>
 
-     
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <div className="head-mod">
+            <div>زمان بازی</div>
+            <div className="time-selector">
+              <div
+                className="time-box"
+                onWheel={handleWheelMinutes}
+                onTouchMove={handleTouchMoveMinutes}
+              >
+                <div className="time-display">
+                  <div className="time-number less">
+                    {formatTime(minutes === 0 ? 10 : minutes - 1)}
+                  </div>
+                  <div className="time-number active">{formatTime(minutes)}</div>
+                  <div className="time-number less">
+                    {formatTime(minutes === 10 ? 0 : minutes + 1)}
+                  </div>
+                </div>
+              </div>
+              <span className="colon">:</span>
+              <div
+                className="time-box"
+                onWheel={handleWheelSeconds}
+                onTouchMove={handleTouchMoveSeconds}
+              >
+                <div className="time-display">
+                  <div className="time-number less">
+                    {formatTime(seconds === 0 ? 59 : seconds - 1)}
+                  </div>
+                  <div className="time-number active">{formatTime(seconds)}</div>
+                  <div className="time-number less">
+                    {formatTime(seconds === 59 ? 0 : seconds + 1)}
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="modal-buttons">
+              <button className="start-btn" onClick={handleStart}>
+                شروع
+              </button>
+              <button className="back-btn" onClick={() => setShowModal(false)}>
+                بازگشت
+              </button>
+            </div>
+            </div>
+          </div>
+        </div>
+      )}
     </>
   );
 }
