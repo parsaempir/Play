28ae235235b3d199a8b2e3357b9a22f47ae415e8
parsaempir <parsa.aysham54@gmail.com>
2025-01-18 04:18:40 +0330

    ..

--- src/Chooseyourcard.js
+++ src/Chooseyourcard.js
@@ -1,44 +1,100 @@
-import './Chooseyourcard.css';
-import giah from './logo gia.png';
-import backlogo from './Group 12.png';
-import Zarb from './Vector.svg';
-import Back from './Group.svg';
-import eye from './Logo.svg';
+import React, { useState } from "react";
+import "./Chooseyourcard.css";
+import giah from "./logo gia.png";
+import backlogo from "./Group 12.png";
+import Zarb from "./Vector.svg";
+import Back from "./Group.svg";
+import eye from "./Logo.svg";
 
-function Chooseyourcard(){
+function Chooseyourcard() {
+  const [showModal, setShowModal] = useState(false);
+  const [minutes, setMinutes] = useState(5);
+  const [seconds, setSeconds] = useState(0);
 
-return(
+  const handleWheelMinutes = (event) => {
+    if (event.deltaY < 0) {
+      setMinutes((prev) => (prev + 1 > 10 ? 10 : prev + 1)); // محدود کردن دقیقه‌ها به ۱۰
+    } else {
+      setMinutes((prev) => (prev - 1 < 0 ? 0 : prev - 1));
+    }
+  };
 
+  const handleWheelSeconds = (event) => {
+    if (event.deltaY < 0) {
+      setSeconds((prev) => (prev + 1 > 59 ? 0 : prev + 1));
+    } else {
+      setSeconds((prev) => (prev - 1 < 0 ? 59 : prev - 1));
+    }
+  };
 
-<>
-<div className='head'>
+  const handleStart = () => {
+    setShowModal(false);
+  };
 
-<div className='container'>
-<span className="head-bz">
-            <img src={Back} className="back"  />
-            <img src={Zarb} className="zarb" />
-
+  return (
+    <>
+      <div className="head">
+        <div className="container">
+          <span className="head-bz">
+            <img src={Back} className="back" alt="back" />
+            <img src={Zarb} className="zarb" alt="zarb" />
           </span>
-          <img src={eye} className='eye' />
-<h2 className='titr'>,نوبت</h2>
-<img src={backlogo} className='back-logo'/>
-<div className='but-div'>
-<button className='but-top'>شروع</button>
-
+          <img src={eye} className="eye" alt="eye" />
+          <h2 className="titr">,نوبت</h2>
+          <img src={backlogo} className="back-logo" alt="back-logo" />
+          <div className="but-div">
+            <button className="but-top" onClick={() => setShowModal(true)}>
+              شروع
+            </button>
+          </div>
+          <img src={giah} className="giah-png" alt="giah" />
+        </div>
       </div>
-<img src={giah} className='giah-png'/>
 
-</div>
-
-</div>
-
-
-</>
-
-
-
-)
-
-
+      {showModal && (
+        <div className="modal-overlay">
+          <div className="modal">
+            <h2>زمان بازی</h2>
+            <div className="time-selector">
+              <div className="time-box" onWheel={handleWheelMinutes}>
+                <div className="time-display">
+                  <div className="time-number less">
+                    {minutes === 0 ? 10 : minutes - 1}
+                  </div>
+                  <div className="time-number active">{minutes}</div>
+                  <div className="time-number less">
+                    {minutes === 10 ? 10 : minutes + 1}
+                  </div>
+                </div>
+                <span>دقیقه</span>
+              </div>
+              <span className="colon">:</span>
+              <div className="time-box" onWheel={handleWheelSeconds}>
+                <div className="time-display">
+                  <div className="time-number less">
+                    {seconds === 0 ? 59 : seconds - 1}
+                  </div>
+                  <div className="time-number active">{seconds.toString().padStart(2, "0")}</div>
+                  <div className="time-number less">
+                    {seconds === 59 ? 0 : seconds + 1}
+                  </div>
+                </div>
+                <span>ثانیه</span>
+              </div>
+            </div>
+            <div className="modal-buttons">
+              <button className="start-btn" onClick={handleStart}>
+                شروع
+              </button>
+              <button className="back-btn" onClick={() => setShowModal(false)}>
+                بازگشت
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
+
 export default Chooseyourcard;
