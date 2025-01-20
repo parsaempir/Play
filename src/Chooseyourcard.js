453a4b8cc892a149a2239205afd9e588a16c5fc5
parsaempir <parsa.aysham54@gmail.com>
2025-01-20 04:17:02 +0330

    /.;

--- src/Chooseyourcard.js
+++ src/Chooseyourcard.js
@@ -10,6 +10,9 @@ function Chooseyourcard() {
   const [showModal, setShowModal] = useState(false);
   const [minutes, setMinutes] = useState(0);
   const [seconds, setSeconds] = useState(0);
+  const [isScrollingMinutes, setIsScrollingMinutes] = useState(false);
+  const [isScrollingSeconds, setIsScrollingSeconds] = useState(false);
+
   if (showModal) {
     document.body.style.overflow = "hidden";
   } else {
@@ -17,36 +20,52 @@ function Chooseyourcard() {
   }
 
   const handleWheelMinutes = (event) => {
-    if (event.deltaY < 0) {
-      setMinutes((prev) => (prev + 1 > 10 ? 0 : prev + 1)); 
-    } else {
-      setMinutes((prev) => (prev - 1 < 0 ? 10 : prev - 1));
+    if (!isScrollingMinutes) {
+      setIsScrollingMinutes(true);
+      if (event.deltaY < 0) {
+        setMinutes((prev) => (prev + 1 > 10 ? 0 : prev + 1));
+      } else {
+        setMinutes((prev) => (prev - 1 < 0 ? 10 : prev - 1));
+      }
+      setTimeout(() => setIsScrollingMinutes(false), 100); 
     }
   };
 
   const handleWheelSeconds = (event) => {
-    if (event.deltaY < 0) {
-      setSeconds((prev) => (prev + 1 > 59 ? 0 : prev + 1));
-    } else {
-      setSeconds((prev) => (prev - 1 < 0 ? 59 : prev - 1));
+    if (!isScrollingSeconds) {
+      setIsScrollingSeconds(true);
+      if (event.deltaY < 0) {
+        setSeconds((prev) => (prev + 1 > 59 ? 0 : prev + 1));
+      } else {
+        setSeconds((prev) => (prev - 1 < 0 ? 59 : prev - 1));
+      }
+      setTimeout(() => setIsScrollingSeconds(false), 300); 
     }
   };
 
   const handleTouchMoveMinutes = (event) => {
-    const touch = event.touches[0];
-    if (touch.clientY < window.innerHeight / 2) {
-      setMinutes((prev) => (prev + 1 > 10 ? 0 : prev + 1));
-    } else {
-      setMinutes((prev) => (prev - 1 < 0 ? 10 : prev - 1));
+    if (!isScrollingMinutes) {
+      setIsScrollingMinutes(true);
+      const touch = event.touches[0];
+      if (touch.clientY < window.innerHeight / 2) {
+        setMinutes((prev) => (prev + 1 > 10 ? 0 : prev + 1));
+      } else {
+        setMinutes((prev) => (prev - 1 < 0 ? 10 : prev - 1));
+      }
+      setTimeout(() => setIsScrollingMinutes(false), 100); 
     }
   };
 
   const handleTouchMoveSeconds = (event) => {
-    const touch = event.touches[0];
-    if (touch.clientY < window.innerHeight / 2) {
-      setSeconds((prev) => (prev + 1 > 59 ? 0 : prev + 1));
-    } else {
-      setSeconds((prev) => (prev - 1 < 0 ? 59 : prev - 1));
+    if (!isScrollingSeconds) {
+      setIsScrollingSeconds(true);
+      const touch = event.touches[0];
+      if (touch.clientY < window.innerHeight / 2) {
+        setSeconds((prev) => (prev + 1 > 59 ? 0 : prev + 1));
+      } else {
+        setSeconds((prev) => (prev - 1 < 0 ? 59 : prev - 1));
+      }
+      setTimeout(() => setIsScrollingSeconds(false), 300); 
     }
   };
 
@@ -64,9 +83,9 @@ function Chooseyourcard() {
             <img src={Back} className="back" />
             <img src={close} className="close" />
           </span>
-          <img src={eye} className="eye"  />
+          <img src={eye} className="eye" />
           <h2 className="titr">نوبت</h2>
-          <img src={backlogo} className="back-logo"  />
+          <img src={backlogo} className="back-logo" />
           <div className="but-div">
             <button className="but-top" onClick={() => setShowModal(true)}>
               شروع
@@ -80,48 +99,48 @@ function Chooseyourcard() {
         <div className="modal-overlay">
           <div className="modal">
             <div className="head-mod">
-            <div className="text-heder">زمان بازی</div>
-            <div className="time-selector">
-              <div
-                className="time-box"
-                onWheel={handleWheelMinutes}
-                onTouchMove={handleTouchMoveMinutes}
-              >
-                <div className="time-display">
-                  <div className="time-number less">
-                    {formatTime(minutes === 0 ? 10 : minutes - 1)}
+              <div className="text-heder">زمان بازی</div>
+              <div className="time-selector">
+                <div
+                  className="time-box"
+                  onWheel={handleWheelMinutes}
+                  onTouchMove={handleTouchMoveMinutes}
+                >
+                  <div className="time-display">
+                    <div className="time-number less">
+                      {formatTime(minutes === 0 ? 10 : minutes - 1)}
+                    </div>
+                    <div className="time-number active">{formatTime(minutes)}</div>
+                    <div className="time-number less">
+                      {formatTime(minutes === 10 ? 0 : minutes + 1)}
+                    </div>
                   </div>
-                  <div className="time-number active">{formatTime(minutes)}</div>
-                  <div className="time-number less">
-                    {formatTime(minutes === 10 ? 0 : minutes + 1)}
+                </div>
+                <span className="colon">:</span>
+                <div
+                  className="time-box"
+                  onWheel={handleWheelSeconds}
+                  onTouchMove={handleTouchMoveSeconds}
+                >
+                  <div className="time-display">
+                    <div className="time-number less">
+                      {formatTime(seconds === 0 ? 59 : seconds - 1)}
+                    </div>
+                    <div className="time-number active">{formatTime(seconds)}</div>
+                    <div className="time-number less">
+                      {formatTime(seconds === 59 ? 0 : seconds + 1)}
+                    </div>
                   </div>
                 </div>
               </div>
-              <span className="colon">:</span>
-              <div
-                className="time-box"
-                onWheel={handleWheelSeconds}
-                onTouchMove={handleTouchMoveSeconds}
-              >
-                <div className="time-display">
-                  <div className="time-number less">
-                    {formatTime(seconds === 0 ? 59 : seconds - 1)}
-                  </div>
-                  <div className="time-number active">{formatTime(seconds)}</div>
-                  <div className="time-number less">
-                    {formatTime(seconds === 59 ? 0 : seconds + 1)}
-                  </div>
-                </div>
+              <div className="modal-buttons">
+                <button className="start-btn" onClick={handleStart}>
+                  شروع
+                </button>
+                <button className="back-btn" onClick={() => setShowModal(false)}>
+                  بازگشت
+                </button>
               </div>
-            </div>
-            <div className="modal-buttons">
-              <button className="start-btn" onClick={handleStart}>
-                شروع
-              </button>
-              <button className="back-btn" onClick={() => setShowModal(false)}>
-                بازگشت
-              </button>
-            </div>
             </div>
           </div>
         </div>
