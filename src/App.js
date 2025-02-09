a972e1f1b41306379b700574d563fa220e86c5ca
parsaempir <parsa.aysham54@gmail.com>
2025-02-09 05:46:31 +0330

    ./

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import SplashScren from './Pageone/SplashScren';
 import Home from './pagetow/Home';
 import Help from './Pagetree/Help';
@@ -7,8 +7,53 @@ import StatusPage from './pagefour/StatusPage';
 import Chooseyourcard from './pagefive/Chooseyourcard';
 import DiscussPage from './Pagesix/DiscussPage';
 import DiscussPagetwo from './Pageseven/discussPagetwo';
-const App = () => {
 
+const resources = [
+  ".jpg",
+ 
+  "./Pagesix/DiscussPage.js",
+  "./Pagesix/DiscussPage.css",
+  "./Pageseven/discussPagetwo.js",
+  "./Pageseven/discussPagetwo.css",
+  "./pagefive/Chooseyourcard.js",
+  "./pagefive/Chooseyourcard.css",
+  "./pagefour/StatusPage.js",
+  "./pagefour/StatusPage.css",
+  "./Pagetree/Help.js",
+  "./Pagetree/Help.css",
+  "./pagetow/Home.js",
+  "./pagetow/Home.css",
+  "./Pageone/SplashScren.js",
+  "./Pageone/SplashScren.css",
+
+
+
+];
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const [loadedResources, setLoadedResources] = useState(0);
+
+  useEffect(() => {
+    if (loadedResources === resources.length) {
+      setLoading(false);
+    }
+  }, [loadedResources]);
+
+  useEffect(() => {
+    resources.forEach((src) => {
+      if (src.endsWith('.jpg') || src.endsWith('.png')) {
+        const img = new Image();
+        img.src = src;
+        img.onload = () => setLoadedResources((prev) => prev + 1);
+      } else {
+        fetch(src).then(() => setLoadedResources((prev) => prev + 1));
+      }
+    });
+  }, []);
+
+  if (loading) {
+    return <div>در حال بارگذاری کل صفحه...</div>;
+  }
   
   return (
     <BrowserRouter>
